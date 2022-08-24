fa009aaa05374e5d4d7179a1e0f59dfb1549b3d4
Reshail Khan <contact@reshailkhan.com>
2022-08-24 08:49:11 +0500

    Files Updated

---
+++ main.js
@@ -0,0 +1,31 @@
+
+$( function() {
+  $( "#tabs" ).tabs();
+});
+
+
+  $(document).on("click",".ui-tabs-anchor-1",function() {
+   $(".ui-tab").removeClass('active');
+     $(this).parent().addClass('active');
+  });
+  $(document).on("click",".ui-tabs-anchor-2",function() {
+      $(".ui-tab").removeClass('active');
+        $(this).parent().addClass('active');
+  });
+  $(document).on("click",".ui-tabs-anchor-3",function() {
+      $(".ui-tab").removeClass('active');
+        $(this).parent().addClass('active');
+  });
+  $(document).on("click",".ui-tabs-anchor-4",function() {
+      $(".ui-tab").removeClass('active');
+        $(this).parent().addClass('active');
+  });
+  $(document).on("click",".ui-tabs-anchor-5",function() {
+      $(".ui-tab").removeClass('active');
+        $(this).parent().addClass('active');
+  });
+  $(document).on("click",".ui-tabs-anchor-6",function() {
+      $(".ui-tab").removeClass('active');
+        $(this).parent().addClass('active');
+  });
+  
